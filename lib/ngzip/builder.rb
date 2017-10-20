a54314379b961f94a6b25eb2876e1e3dd15d79cf
dup2 <zarkov@cargoserver.ch>
2017-10-20 21:56:33 +0200

    whitespace fix

--- lib/ngzip/builder.rb
+++ lib/ngzip/builder.rb
@@ -2,18 +2,16 @@ require 'zlib'
 require 'uri'
 
 module Ngzip
-  
   class Builder
-    
     BUFFER_SIZE = 8 * 1024
-    
+
     # Public: Build the files manifest for mod_zip, see http://wiki.nginx.org/NginxNgxZip for the specs.
-    # 
+    #
     # files - An Array of absolute file path elements
     # options - The options, see below
-    # 
+    #
     # Returns a line for each file separated by \n
-    #  
+    #
     # The following options are available:
     #  :crc         => Enable or disable CRC-32 checksums
     #  :crc_cache   => Allows for provided cached CRC-32 checksums in a hash where the key is the file path
@@ -21,7 +19,7 @@ module Ngzip
     def build(files, options = {})
       settings = {:crc => true}
       settings.merge! options
-            
+
       list = file_list(files)
       prefix = options[:base_dir] || detect_common_prefix(list)
       prefix += '/' unless prefix.end_with?('/')
@@ -36,26 +34,25 @@ module Ngzip
     end
 
     # Public: Get the special header to signal the mod_zip
-    # 
+    #
     # Returns the header as a string "key: value"
     def header()
       "X-Archive-Files: zip"
     end
-    
-    
-    # Public: Encode the string 
+
+    # Public: Encode the string
     #
     # Returns the encoded string using URL escape formatting
     def self.encode(string)
       URI.encode(string).gsub('+', '%2B').gsub('?', '%3F')
     end
-    
+
     private
-    
+
     # Internal: Compute a common prefix from a list of path elements
-    # 
+    #
     # list - The list of file path elements
-    # 
+    #
     # Returns a common prefix
     def detect_common_prefix(list)
       if list.size == 1
@@ -69,9 +66,9 @@ module Ngzip
       end
       prefix
     end
-    
+
     # Internal: Compute the file list by expanding directories
-    # 
+    #
     def file_list(files)
       Array(files).map do |e|
         if File.directory?(e)
@@ -81,33 +78,30 @@ module Ngzip
         end
       end.flatten
     end
-    
-    
-    # Internal: Compute the CRC-32 checksum for a file unless the settings 
+
+    # Internal: Compute the CRC-32 checksum for a file unless the settings
     # disable the computation (:crc => false) and this method returns "-"
-    # 
+    #
     # file - The full path to the file
     # settings - The settings hash
-    # 
+    #
     # Returns a hex string
     def compute_crc32(file, settings)
       return '-' unless settings[:crc]
-      
+
       # honor the cache
       if settings[:crc_cache] && settings[:crc_cache][file]
         return settings[:crc_cache][file]
       end
-      
+
       # read using a buffer, we might operate on large files!
       crc32 = 0
       File.open(file,'rb') do |f|
         while buffer = f.read(BUFFER_SIZE) do
-          crc32 = Zlib.crc32(buffer, crc32) 
+          crc32 = Zlib.crc32(buffer, crc32)
         end
       end
       crc32.to_s(16)
     end
-    
   end
-  
 end
